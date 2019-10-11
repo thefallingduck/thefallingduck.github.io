21aa1dd7ab10ac3e598a7765703b9c470a5eda43
thefallingduck <bjlinzmeier@gmail.com>
2019-10-10 19:14:44 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,84 @@
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Ben Linzmeier">
+
+    <title>Benjamin J. Linzmeier</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/customstyles.css" rel="stylesheet">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Geochemistry of shells and oceans</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                	<li>
+                		<a href="/about/about.html">About Me</a>
+                	</li>
+                	
+                    <li>
+                        <a href="docs/BJLCV_10_4_2019.pdf" target="_blank">CV</a>
+                    </li>
+                    <li>
+                        <a href="/pubs/pubs.html">Publications</a>
+                    </li>
+                    <li class="dropdown">
+                		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Projects <span class="caret"></span></a>
+                 		 <ul class="dropdown-menu" role="menu">
+                     		<li><a href="/projects/CephalopodEcology.html">Cephalopod Ecology</a></li>
+                    		<li><a href="/projects/QGIS_For_Micro.html">GIS for Microanalysis</a></li>
+                    		<li><a href="/projects/OceanAcidification.html">Ocean Acidification</a></li>
+                    		<li><a href="/projects/future_projects.html">Future Projects</a></li>
+
+                  		</ul>
+                	</li>
+					</li>
+                    <li class="dropdown">
+                		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Teaching <span class="caret"></span></a>
+                 		 <ul class="dropdown-menu" role="menu">
+                     		<li><a href="/projects/TeachingAsOutreach.html">Teaching as outreach</a></li>
+                    		<li><a href="/projects/ResearchWithTeaching.html">Research through teaching</a></li>
+                    		<li><a href="/projects/Mentorship.html">Mentorship</a></li>
+                    		
+                  		</ul>
+                	</li>
+                    <li>
+                        <a href="/contact/contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
